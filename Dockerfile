8168ff5e462e0bae97c1e2d5734be75292eb01cd
thetaru <sensesensesense012345678910@gmail.com>
2022-05-29 03:00:17 +0900

    Update

---
+++ Dockerfile
@@ -0,0 +1,2 @@
+FROM gcc
+RUN apt-get update && apt-get -y install --no-install-recommends gdb
---
+++ ltest.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <linux/if.h>
+#include <net/ethernet.h>
+#include <netpacket/packet.h>
+#include <netinet/if_ether.h>
